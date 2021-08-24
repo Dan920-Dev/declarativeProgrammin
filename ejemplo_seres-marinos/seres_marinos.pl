00ffcda646ecb9d7390d3ea2909fa57f8d24dab8
Dan920-Dev <escobare362@gmail.com>
2021-08-23 21:01:38 -0600

    Añadiendo ejemplo

---
+++ ejemplo_seres-marinos/seres_marinos.pl
@@ -0,0 +1,48 @@
+ser_marino(bruce).
+ser_marino(peach).
+ser_marino(dori).
+ser_marino(nemo).
+ser_marino(marlin).
+ser_marino(planton).
+ser_marino(bob).
+ser_marino(patricio).
+ser_marino(oscar).
+ser_marino(angie).
+ser_marino(lenny).
+ser_marino(don_cangrejo).
+ser_marino(ancla).
+ser_marino(chum).
+
+amigo(bob, patricio).
+amigo(patricio, bob).
+amigo(bob, don_cangrejo).
+amigo(don_cangrejo, bob).
+amigo(bob, calamardo).
+amigo(patricio, calamardo).
+amigo(patricio, don_cangrejo).
+amigo(don_cangrejo, calamardo).
+amigo(don_cangrejo, patricio).
+amigo(ancla, bruce).
+amigo(bruce, ancla).
+amigo(ancla, chum).
+amigo(chum, ancla).
+amigo(bruce, chum).
+amigo(marlin, dori).
+amigo(dori, marlin).
+
+enemigo(don_cangrejo, planton).
+enemigo(planton, don_cangrejo).
+
+pasatiempo(bob, pescar_meduzas).
+pasatiempo(patricio, pescar_meduzas).
+pasatiempo(calamardo, tocar_clarinete).
+pasatiempo(don_cangrejo, hacer_dinero).
+
+trabaja_en(bob, crustaceo_cascarudo).
+trabaja_en(calamardo, crustaceo_cascarudo).
+trabaja_en(don_cangrejo, crustaceo_cascarudo).
+trabaja_en(oscar, lavado_de_ballenas).
+trabaja_en(angie, lavado_de_ballenas).
+trabaja_en(senora_puff, escuela_de_manejo).
+
+
